6f5cc6c593211a33779f00c19a35e1f2e25bc120
smt <smtea923@gmail.com>
2019-10-05 14:11:39 +0100

    Add classes for items and basic player interaction

---
+++ Assets/Scripts/Entities/Living/Living.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Living : BaseObject
+{
+    public float StartingHealth;
+    private float Health;
+
+    private void Start() {
+        Health = StartingHealth;
+    }
+
+    public float GetHealth()
+    {
+        return Health;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Debug.Log(transform.name + " damaged for: " + damage);
+        float resulthp = Health - damage;
+        if (resulthp <= 0.0f)
+        {
+            Health = 0.0f;
+            Debug.Log(transform.name + " dead!");
+        }
+        else
+        {
+            Health = resulthp;
+        }
+    }
+}
