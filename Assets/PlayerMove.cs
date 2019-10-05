8ad8b38faffe7b458679ad358f0da274d026ae17
smt <smtea923@gmail.com>
2019-10-05 02:29:06 +0100

    Add basic character prefab

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController cc;
+
+    public float MoveSpeed;
+    public float Gravity;
+    
+    private Vector3 moveVector = Vector3.zero;
+    
+    void Start()
+    {
+        cc  = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        if (cc.isGrounded)
+        {
+            moveVector = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        }
+
+        moveVector.y -= Gravity * Time.deltaTime;
+
+        cc.Move(moveVector.normalized * MoveSpeed * Time.deltaTime);
+    }
+}
