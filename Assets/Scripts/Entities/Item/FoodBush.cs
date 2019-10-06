493d73bef1aaa665ec9e2fe1e3645d67da5f7c97
smt <smtea923@gmail.com>
2019-10-06 14:22:19 +0100

    Add foodbush

---
+++ Assets/Scripts/Entities/Item/FoodBush.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBush : Item
+{
+   public float FoodRestored = 25.0f;
+
+   public override void Interact(Player player)
+   {
+       player.EatFood(FoodRestored);
+       Destroy(gameObject);
+   }
+}
