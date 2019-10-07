b78a5c6651e66e99544d9394fb36cbdd1832abc2
Peter Welby <pwelby.mauz@gmail.com>
2019-10-06 20:47:00 -0400

    Prototype prefabs for interactable objectives

---
+++ Assets/Scripts/Entities/Item/Oven.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven : Item
+{
+    bool objectiveCompleted = false;
+
+    public override void Interact(Player player)
+    {
+        if (
+        !objectiveCompleted &&
+        player.PlayerInv.Water > 0 &&
+        player.PlayerInv.Flour >= 6
+        )
+        {
+            GameObject.Instantiate(DropPrefab, transform.position + transform.forward, transform.rotation);
+        }
+    }
+}
