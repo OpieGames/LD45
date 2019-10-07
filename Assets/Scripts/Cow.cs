d21a572011b3f96c7f34b98fe5d68ca8a3cfc87c
Peter Welby <pwelby.mauz@gmail.com>
2019-10-07 12:49:10 -0400

    Add cow and cow pen behavior

---
+++ Assets/Scripts/Cow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    public float collisionDamage = 1.0f;
+    public GameObject dropPrefab;
+
+    Collider myCollider;
+    bool hasDroppedMilk = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<BoxCollider>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player playerComponent = collision.gameObject.GetComponent<Player>();
+            playerComponent.TakeDamage(collisionDamage);
+        }
+    }
+
+    // Drop the milk prefab. This is called by the pen trigger volume when a cow enters it.
+    public void DropItem()
+    {
+        if (!hasDroppedMilk)
+        {
+            Instantiate(dropPrefab, transform.position - transform.forward * 2.0f, transform.rotation);
+            hasDroppedMilk = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/CowPen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowPen : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cow"))
+        {
+            Debug.Log("Cow entered pen trigger");
+            Cow cowComponent = other.gameObject.GetComponent<Cow>();
+            cowComponent.DropItem();
+        }
+    }
+}
